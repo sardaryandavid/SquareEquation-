abab82ae7aae9fd3e0456f7f567e77475a468470
Sardaryan David <sardaryan.david04@mail.ru>
2021-08-20 13:34:52 +0300

    8 version

--- SquareEquation_2.c
+++ SquareEquation_2.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#define INF 1e7
+#include <assert.h>
 
-void SolveSquareEquation(double a, double b, double c, double *x1, double *x2, int *roots_number);
+#define INF -1
+
+int SolveSquareEquation(double a, double b, double c, double *x1, double *x2);
 int equal(double a, double b);
 int isZero(double a);
-void print(double *x1, double *x2, int *roots_number);
+void print_roots(double x1, double x2, int roots_number);
 
 int main()
 {
     double a = 0;
     double b = 0;
     double c = 0;
+
     double x1 = 0;
     double x2 = 0;
+
     int roots_number = 0;
-    printf("This program solve equation of the following type: ax^2 + bx + c = 0\n");
+
+    printf("This program solves equations of the following type: ax^2 + bx + c = 0\n");
     printf("Please, enter:\n");
 
     printf("a = ");
@@ -26,37 +31,38 @@ int main()
     printf("c = ");
     scanf("%lf", &c);
 
-    SolveSquareEquation(a, b, c, &x1, &x2, &roots_number);
+    roots_number = SolveSquareEquation(a, b, c, &x1, &x2);
 
-    print(&x1, &x2, &roots_number);
+    print_roots(x1, x2, roots_number);
 
     return 0;
 }
 
-void SolveSquareEquation(double a, double b, double c,double *x1, double *x2, int *roots_number) {
+int SolveSquareEquation(double a, double b, double c,double *x1, double *x2) {
+    int roots_number = 0;
     if (isZero(a)) {
         if (isZero(b)) {
             if (isZero(c))
-               *roots_number = INF;
+               roots_number = INF;
             else
-               *roots_number = 0;
+               roots_number = 0;
         }
         else
-            *roots_number = 1;
-            *x1 = *x2 = -c / a;
+            roots_number = 1;
+            *x1 = *x2 = -c / b;
     }
 
     else {
         if (isZero(b)) {
             if (isZero(c)) {
-                *roots_number = 1;
+                roots_number = 1;
                 *x1 = *x2 = 0;
             }
             else {
                 if (c / a < -1e-6)
-                    *roots_number = 0;
+                    roots_number = 0;
                 else {
-                    *roots_number = 2;
+                    roots_number = 2;
                     *x1 = c / a;
                     *x2 = -c / a;
                 }
@@ -64,40 +70,43 @@ void SolveSquareEquation(double a, double b, double c,double *x1, double *x2, in
         }
         else {
             double discriminant = b * b - 4 * a * c;
-            if (isZero(discriminant)) {
-                *roots_number = 1;
-                *x1 = *x2 = -b / (2 * a);
-            }
             if (discriminant < -1e-6)
-                *roots_number = 0;
-            if (discriminant > 1e-6) {
-                *roots_number = 2;
+                roots_number = 0;
+            if (discriminant >= -1e-6) {
                 *x1 = (-b + sqrt(discriminant))/ (2 * a);
                 *x2 = (-b - sqrt(discriminant))/ (2 * a);
+                if(!equal(*x1, *x2))
+                    roots_number = 2;
+                else
+                    roots_number = 1;
             }
         }
 
     }
+    return roots_number;
 }
 
 int equal(double a, double b) {
-    if (fabs(a - b) <= 1e-6)
-        return 1;
-    else
-        return 0;
+    return (fabs(a - b) <= 1e-6);
 }
 
 int isZero(double a) {
     return equal(a, 0);
 }
 
-void print(double *x1, double *x2, int *roots_number) {
-    if (*roots_number == 0)
+void print_roots(double x1, double x2, int roots_number) {
+    switch(roots_number) {
+    case 0:
         printf("No roots");
-    if (*roots_number == 1)
-        printf("x1 = x2 = %lf", *x1);
-    if (*roots_number == 2)
-        printf("x1 = %lf, x2 = %lf", *x1, *x2);
-    if (*roots_number == INF)
+        break;
+    case 1:
+        printf("x1 = x2 = %lf", x1);
+        break;
+    case 2:
+        printf("x1 = %lf, x2 = %lf", x1, x2);
+        break;
+    case -1:
         printf("Unlimited number of roots");
+        break;
+    }
 }
