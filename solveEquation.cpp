09cfdca082c9a16b3daeb7def35d162cdef8130e
sardaryandavid <74458820+sardaryandavid@users.noreply.github.com>
2021-08-22 22:53:27 +0300

    Add files via upload

--- solveEquation.cpp
+++ solveEquation.cpp
@@ -6,21 +6,16 @@ const double PRECISION = 1e-6;
 enum roots_number {infRoots = -1, noRoots = 0, oneRoot = 1, twoRoots = 2};
 
 int isZero(double a);
+int solveLinearEquation(double a, double b, double *x);
 
-int solveSquareEquation(double a, double b, double c, double *x1, double *x2) {
+int solveSquareEquation(double a, double b, double c, double *x1 = 0, double *x2 = 0) {
     assert(x1 != nullptr && x2 != nullptr);
     assert(std::isfinite(a) && std::isfinite(b) && std::isfinite(b));
 
     if (isZero(a)) {
-        if (isZero(b)) {
-            if (isZero(c))
-               return infRoots;
-
-            return noRoots;
-        }
-
-        *x1 = *x2 = -c / b;
-        return oneRoot;
+        int roots_number = solveLinearEquation(b, c, x1);
+        *x2 = *x1;
+        return roots_number;
     }
 
     if (isZero(b)) {
@@ -45,11 +40,23 @@ int solveSquareEquation(double a, double b, double c, double *x1, double *x2) {
 
     *x1 = (-b + sqrt(discriminant))/ (2 * a);
     *x2 = (-b - sqrt(discriminant))/ (2 * a);
-    if (!isZero(discriminant)) {
-        return twoRoots;
+    if (isZero(discriminant)) {
+        return oneRoot;
     }
 
-    return oneRoot;
+    return twoRoots;
+}
+
+int solveLinearEquation(double a, double b, double *x) {
+    if(a == 0) {
+        if(b == 0)
+            return infRoots;
+
+        return noRoots;
+    }
+
+    *x = -b / a;
+    return 1;
 }
 
 int isZero(double a) {
